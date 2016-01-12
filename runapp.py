6f9ba9cb75e61cf42e8b0f4302914e13955cb55d
Alexis Métaireau <alexis@notmyidea.org>
2016-01-12 19:16:46 +0100

    Raise an error when starting the app

--- runapp.py
+++ runapp.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     os.setenv("KINTO_CACHE_URL", os.getenv("DATABASE_URL"))
     os.setenv("KINTO_STORAGE_URL", os.getenv("DATABASE_URL"))
     os.setenv("KINTO_PERMISSION_URL", os.getenv("DATABASE_URL"))
+    raise ValueError(os.getenv("DATABASE_URL"))
     app = loadapp('config:kinto.ini', relative_to='.')
 
     serve(app, host='0.0.0.0', port=port)
