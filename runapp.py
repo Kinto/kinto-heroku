c258425a170b3078ac9964646641102fcda510f5
Alexis Métaireau <alexis@notmyidea.org>
2016-01-12 19:00:52 +0100

    Add missing files

---
+++ runapp.py
@@ -0,0 +1,14 @@
+import os
+
+from paste.deploy import loadapp
+from waitress import serve
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
+    os.setenv("KINTO_CACHE_URL", os.getenv("DATABASE_URL"))
+    os.setenv("KINTO_STORAGE_URL", os.getenv("DATABASE_URL"))
+    os.setenv("KINTO_PERMISSION_URL", os.getenv("DATABASE_URL"))
+    app = loadapp('config:kinto.ini', relative_to='.')
+
+    serve(app, host='0.0.0.0', port=port)
