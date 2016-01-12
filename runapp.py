c3f04dc0ae2148bd0295c380864a7ea7d2451153
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-01-12 19:49:29 +0100

    Raise before

--- runapp.py
+++ runapp.py
@@ -7,10 +7,10 @@ from waitress import serve
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
+    raise ValueError(os.getenv("DATABASE_URL"))
     os.setenv("KINTO_CACHE_URL", os.getenv("DATABASE_URL"))
     os.setenv("KINTO_STORAGE_URL", os.getenv("DATABASE_URL"))
     os.setenv("KINTO_PERMISSION_URL", os.getenv("DATABASE_URL"))
-    raise ValueError(os.getenv("DATABASE_URL"))
     app = loadapp('config:kinto.ini', relative_to='.')
 
     serve(app, host='0.0.0.0', port=port)
